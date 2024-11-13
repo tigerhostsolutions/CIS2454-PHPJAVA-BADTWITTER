6c9faef51ad7873fc06e7762402fdff86f8de97f
TigerHost <jgrasty@tigerhost.domains>
2024-11-13 16:59:21 -0500

    Created SpringBoot Project

---
+++ badtwitter/src/main/java/domains/tigerhost/badtwitter/BadtwitterApplication.java
@@ -0,0 +1,13 @@
+package domains.tigerhost.badtwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadtwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadtwitterApplication.class, args);
+	}
+
+}
---
+++ badtwitter/src/test/java/domains/tigerhost/badtwitter/BadtwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package domains.tigerhost.badtwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadtwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
